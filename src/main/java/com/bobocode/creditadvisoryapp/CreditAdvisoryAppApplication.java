634a6e0dac94a8f4c907d85609ee87845c109f83
yy <eevgeniy@gmail.com>
2022-09-29 07:36:55 +0300

    Init commit

---
+++ src/main/java/com/bobocode/creditadvisoryapp/CreditAdvisoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.bobocode.creditadvisoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAdvisoryAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreditAdvisoryAppApplication.class, args);
+  }
+
+}
